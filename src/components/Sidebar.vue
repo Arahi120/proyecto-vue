ad594dabdd2f4e527d6c100c46411718d69cbe4b
arahi <a22311144@uthermosillo.edu.mx>
2025-02-03 19:36:51 -0700

    Crea componente Lista de tareas, borra coponentes autogenerados, crea rutas

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+<div class=" sidebar">
+    <h2>Barra lateral</h2>
+    <ul>
+    <li><Router-link to="/contador">Contador</Router-link></li>
+    <li><Router-link to="/lista-de-tareas">Lista de Tareas</Router-link></li>
+    </ul>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.sidebar {
+    width: 200px;
+    color: rgb(46, 46, 36);
+    padding: 20px;
+}
+</style>
