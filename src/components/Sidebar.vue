9f2ba9352e26dc1c800c2e9228cc3f5ab6e686d5
arahi <a22311144@uthermosillo.edu.mx>
2025-02-04 22:19:28 -0700

    hace que el header muestre el clima actual utilizando una API

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,8 +1,8 @@
 <template>
 <div class=" sidebar">
     <h2>Bienvenido</h2>
-    <h3>Nombre: {{ nombre }}</h3>
-    <h3>Email: {{ email }}</h3>
+    <h3>Nombre: {{ registrarStore.nombre }}</h3>
+    <h3>Email: {{ registrarStore.email }}</h3>
     <ul>
     <li><Router-link to="/contador">Contador</Router-link></li>
     <li><Router-link to="/lista-de-tareas">Lista de Tareas</Router-link></li>
@@ -16,15 +16,9 @@
 import { useRegistrarStore } from '@/modules/registro/stores/registrarStore';
 import {ref, watch} from 'vue';
 const registrarStore = useRegistrarStore();
-const nombre = ref(registrarStore.nombre.value);
-const email = ref(registrarStore.email.value);
-watch(() => registrarStore.nombre, (newValue) => {
-    nombre.value = newValue;
-});
+const nombre = ref();
+const email = ref();
 
-watch(() => registrarStore.email, (newValue) => {
-    email.value = newValue;
-})
 
 </script>
 
---
+++ src/components/Weather.vue
@@ -0,0 +1,19 @@
+<template>
+<h3>Clima de hoy: {{ weatherStore.temperatura }}°</h3>
+</template>
+
+<script setup>
+import { useWeather } from '../composables/useWeather';
+import { onMounted } from 'vue';
+import { useWeatherStore } from '../stores/weatherStore';
+
+const weatherStore = useWeatherStore();
+
+onMounted(() => {
+    useWeather();
+})
+</script>
+
+<style>
+
+</style>
